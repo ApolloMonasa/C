6a2d21bd80e99a500246bf8ae805422badb2e5c1
ApolloMonasa <2784761245@qq.com>
2025-06-05 21:33:52 +0800

    feat : add .cpp about inherit_pointer

--- Cpp_FirstSight/C2_Inheritance/3.inherit_pointer.cpp
+++ Cpp_FirstSight/C2_Inheritance/3.inherit_pointer.cpp
@@ -38,10 +38,26 @@ void func1(Base *p) {
     cout << "Base : " << p->x <<  ", " << p->y << endl;
     return ;
 }
+/*Chapter 4 Inheritance
+ * Base pointer to Uperclass
+ * 子类只是父类的拓展
+ * 所有用在父类上的方法都能用在子类上
+ *
+ */
+
+ostream &operator<<(ostream &out, const Base &obj) {
+    out << "Base (" << obj.x << ", " << obj.y << ")" << endl;
+    return out;
+}
 
 int main() {
     A a(3, 4, 5), *p = &a;//子类的地址可以隐式转换为父类的地址
-    func1(p);
     Base &b = a;//子类对象可以绑定到父类引用
+    func1(&a);
+    func1(p);
+    b.x = 5;
+    b.y = 6;
+    func1(&b);
+    cout << a << b;
     return 0;
 }
